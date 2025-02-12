08f43997b1f593a4294f6b5fd92c04fef27aa220
realhuhu <2661467107@qq.com>
2025-02-12 19:48:43 +0800

    onnx

--- main.cpp
+++ main.cpp
@@ -16,12 +16,13 @@ bool mostAngle = true;
 int main() {
     SetConsoleOutputCP(CP_UTF8);
     std::cout << "开始" << std::endl;
-    auto ocr = OcrLite();
+
+    OcrLite ocr;
     ocr.initModels(
-            "models2/det.onnx",
-            "models2/cls.onnx",
-            "models2/rec.onnx",
-            "models2/keys.txt"
+            "models/det.onnx",
+            "models/cls.onnx",
+            "models/rec.onnx",
+            "models/keys.txt"
     );
     ocr.setNumThread(1);
     std::cout << "模型初始化完成" << std::endl;
@@ -31,35 +32,33 @@ int main() {
     std::cin >> imgPath;
 
     auto mat = cv::imread(imgPath);
-//    auto mat = cv::imread("C:/Users/huhu/Desktop/test.png");
+
     std::cout << "图片已读取" << std::endl;
-    while (true){
-        auto startTime = std::chrono::steady_clock::now();
-        auto res = ocr.detect(
-                mat,
-                padding,
-                maxSideLen,
-                boxScoreThresh,
-                boxThresh,
-                unClipRatio,
-                doAngle,
-                mostAngle
-        );
-        auto endTime = std::chrono::steady_clock::now();
+    auto startTime = std::chrono::steady_clock::now();
+    auto res = ocr.detect(
+            mat,
+            padding,
+            maxSideLen,
+            boxScoreThresh,
+            boxThresh,
+            unClipRatio,
+            doAngle,
+            mostAngle
+    );
+    auto endTime = std::chrono::steady_clock::now();
 
-        std::cout << "OCR完成" << std::endl;
+    std::cout << "OCR完成" << std::endl;
 
-        for (const auto &i: res.textBlocks) {
-            std::cout << i.text.c_str() << ": ";
-            for (auto j: i.boxPoint) {
-                std::cout << j;
-            }
-            std::cout << std::endl;
+    for (const auto &i: res.textBlocks) {
+        std::cout << i.text.c_str() << ": ";
+        for (auto j: i.boxPoint) {
+            std::cout << j;
         }
-
-        std::chrono::duration<double, std::milli> duration = endTime - startTime;
-        std::cout << "用时" << std::to_string(duration.count()) << "毫秒" << std::endl;
+        std::cout << std::endl;
     }
+
+    std::chrono::duration<double, std::milli> duration = endTime - startTime;
+    std::cout << "用时" << std::to_string(duration.count()) << "毫秒" << std::endl;
 
     system("pause");
     return 0;
--- src/AngleNet.h
+++ src/AngleNet.h
@@ -2,7 +2,7 @@
 #define __OCR_ANGLENET_H__
 
 #include "OcrStruct.h"
-#include <onnxruntime/core/session/onnxruntime_cxx_api.h>
+#include <onnxruntime_cxx_api.h>
 #include <opencv2/opencv.hpp>
 
 class AngleNet {
@@ -22,13 +22,13 @@ public:
 private:
     bool isOutputAngleImg = false;
 
-    Ort::Session *session;
+    Ort::Session *session{};
     Ort::Env env = Ort::Env(ORT_LOGGING_LEVEL_ERROR, "AngleNet");
     Ort::SessionOptions sessionOptions = Ort::SessionOptions();
     int numThread = 0;
 
-    std::vector<Ort::AllocatedStringPtr> inputNamesPtr;
-    std::vector<Ort::AllocatedStringPtr> outputNamesPtr;
+    std::vector<Ort::AllocatedStringPtr> inputNamesPtr{};
+    std::vector<Ort::AllocatedStringPtr> outputNamesPtr{};
 
     const float meanValues[3] = {127.5, 127.5, 127.5};
     const float normValues[3] = {1.0 / 127.5, 1.0 / 127.5, 1.0 / 127.5};
